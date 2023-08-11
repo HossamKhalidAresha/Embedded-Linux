aee45d481ab7da4b68c7dbee35290cac69938c85
HossamKhalidAresha <141756172+HossamKhalidAresha@users.noreply.github.com>
2023-08-11 10:08:09 +0300

    Delete Vowel_Checking.py

--- Python/Vowel_Checking.py
+++
@@ -1,6 +0,0 @@
-vowel="eaiou"
-letter=input("please,enter a letter : ")
-if(vowel.find(letter.lower())!=-1):
-    print("{} is a vowel".format(letter))
-else:
-     print("{} is not a vowel".format(letter))
